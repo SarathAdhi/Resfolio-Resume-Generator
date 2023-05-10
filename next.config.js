215e37d21d8c2413ff1fd455c251d73a7511caaf
SarathAdhi <s.sarathadhithya@gmail.com>
2023-05-10 20:15:03 +0530

    facebook pixel analytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_FACEBOOK_PIXEL_ID: process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,30 @@ import "rsuite/dist/rsuite.min.css";
 import type { AppProps } from "next/app";
 import { CustomProvider } from "rsuite";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const [hydrated, setHydrated] = useState(false);
 
   useEffect(() => {
     setHydrated(true);
   }, []);
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(FB_PIXEL_ID!);
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
 
   if (!hydrated) return <></>;
 
