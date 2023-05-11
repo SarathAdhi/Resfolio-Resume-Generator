5ab353ab25c3bfd8ce994912ba3992562c4adcb8
SarathAdhi <s.sarathadhithya@gmail.com>
2023-05-11 15:57:26 +0530

    bug: minor change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
   }
 
   useEffect(() => {
-    if (user) appLoadingFetchAction();
+    appLoadingFetchAction();
   }, [user]);
 
   useEffect(() => {
